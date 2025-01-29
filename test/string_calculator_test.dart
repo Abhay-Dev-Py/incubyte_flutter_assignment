0c9dd52fe4713c79270cd4c1e5810f0e7c3c29bd
abhay.rajput <abhay.rajput@ppreciate.com>
2025-01-29 14:48:51 +0530

    Add initial implementation of StringCalculator with empty string test
    - Create StringCalculator class with add method
    - Implement test case for empty string input returning 0
    - Set up basic test structure for StringCalculator

---
+++ test/string_calculator_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:incubyte_flutter_assignment/string_calculator.dart';
+
+void main() {
+  late StringCalculator calculator;
+
+  setUp(() {
+    calculator = StringCalculator();
+  });
+
+  group('StringCalculator', () {
+    test('should return 0 for empty string', () {
+      expect(calculator.add(""), equals(0));
+    });
+  });
+} 
